a6a17af99d968cf594b04c5a0d9cc069be7877ed
Pedro Santos <donbonifacio@gmail.com>
2013-05-14 12:32:33 +0100

    Change color

--- hipchat.js
+++ hipchat.js
@@ -10,7 +10,7 @@ function HipChat(auth_token) {
 }
 
 HipChat.prototype.sendMessage = function(room, message) {
-  var payload = { notify:1, message_format:'text', format: 'json', auth_token: this.auth_token, room_id: room, from: "IX-BOT", message: message }
+  var payload = { notify:1, message_format:'text', format: 'json', auth_token: this.auth_token, room_id: room, from: "IX-BOT", message: message, color: 'red' }
   request.post('https://api.hipchat.com/v1/rooms/message', { form: payload }, function(err, respon, body) {
     if(err) console.log("Error" + err)  
     console.log("Body: " + body)
